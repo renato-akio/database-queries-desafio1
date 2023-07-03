18d9bf85d6b740e861856f327469f63faef769b1
Renato Akio Hossotani <renato.hossotani@indigosoft.com.br>
2023-07-03 12:31:52 -0300

    first commit

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,39 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../../../users/entities/User';
+import { Game } from '../../entities/Game';
+
+import { IGamesRepository } from '../IGamesRepository';
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    const games = await this.repository.createQueryBuilder()
+      .where("title ilike :title", { title: `%${param}%` })
+      .getMany();
+
+    return games;
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+    const countGames = await this.repository.query('SELECT COUNT(id) FROM games');
+    return countGames;
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+    return await this.repository.createQueryBuilder("games")
+      .innerJoinAndSelect("games.users", "users")
+      .getOne()
+      .then(game => {
+        if (!game) {
+          throw new Error("Game not found!")
+        }
+        return game.users
+      });
+  }
+}
